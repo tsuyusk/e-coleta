b8d59bccae175640634c8b684f2778facca77855
tsuyusk <rafaeldesenhos99@gmail.com>
2020-06-01 12:04:35 -0300

    frontend added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
 import express from "express";
+
+const app = express();
+const port = 3333;
+
+app.get("/users", (req, res) => {
+  res.json({ names: ["tsuy", "luna", "abner"] });
+  console.log("listagem de usuarios");
+});
+
+app.listen(port, () => {
+  console.log(`Listening port ${port}`);
+});
